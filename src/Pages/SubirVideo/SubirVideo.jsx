dddf718b018120c45f7bff0bc23a830dae5b15d0
YossecSA <yosec.arratea22@gmail.com>
2025-01-12 13:50:03 -0500

    preparacion de los componentes de la pantalla

---
+++ src/Pages/SubirVideo/SubirVideo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import sytle from './SubirVideo.module.css'
+
+export default function SubirVideo() {
+  return (
+    <div>
+      <h2>Subir Video</h2>
+    </div>
+  )
+}
--- src/RoutesPrincipal.jsx
+++ src/RoutesPrincipal.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PaginaBase from "./Pages/PaginaBase/PaginaBase";
+import SubirVideo from "./Pages/SubirVideo/SubirVideo";
+import Inicio from "./Pages/Inicio/Inicio";
 
 export default function RoutesPrincipal() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<PaginaBase />}></Route>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />}></Route>
+                    <Route path="subirvideo" element={<SubirVideo />}></Route>
+                </Route>
             </Routes>
         </BrowserRouter>
     );

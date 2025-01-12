a19e3ddc7c56c3ddbee11b454fedfd17f3b585d5
YossecSA <yosec.arratea22@gmail.com>
2025-01-12 12:48:04 -0500

    pagina base

---
+++ routes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function routes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
